2cdfd45fb70825d973a3b416fe86a072054643e5
Sebastien Fauque <sbfauque@gmail.com>
2024-05-07 11:29:04 -0700

    updating the files with the requested contents.

--- lab2/demo2_sebastienfauque.py
+++ lab2/demo2_sebastienfauque.py
@@ -1,26 +1,65 @@
 import pytest
 from sebastienfauqueLab2 import pascals_triangle
 
-def test_pascals_triangle_1():
-    """asserts that output is correct for basecase."""
-    assert pascals_triangle(1) == [1]
+# print("TEST CASES ----------------------")
+# for n in range(1, 7):
+#     print(f"Case {n}: ")
+#     pascals_triangle(n)
 
-def test_pascals_triangle_2():
-    """asserts that the output is correct for n = 2."""
-    assert pascals_triangle(2) == [1, 1]
 
-def test_pascals_triangle_3():
-    """asserts that the output is correct for n = 3."""
-    assert pascals_triangle(3) == [1, 2, 1]
 
-def test_pascals_triangle_4():
-    """asserts that the output is correct for n = 4."""
-    assert pascals_triangle(4) == [1, 3, 3, 1]
+# Stack frame for stack frame up to 6.
+# TEST CASES ----------------------
+# Case 1:
+# row 0  [1]
+# Case 2:
+# row 0  [1]
+# row 1  [1, 1]
+# Case 3:
+# row 0  [1]
+# row 1  [1, 1]
+# row 2  [1, 2, 1]
+# Case 4:
+# row 0  [1]
+# row 1  [1, 1]
+# row 2  [1, 2, 1]
+# row 3  [1, 3, 3, 1]
+# Case 5:
+# row 0  [1]
+# row 1  [1, 1]
+# row 2  [1, 2, 1]
+# row 3  [1, 3, 3, 1]
+# row 4  [1, 4, 6, 4, 1]
+# Case 6:
+# row 0  [1]
+# row 1  [1, 1]
+# row 2  [1, 2, 1]
+# row 3  [1, 3, 3, 1]
+# row 4  [1, 4, 6, 4, 1]
+# row 5  [1, 5, 10, 10, 5, 1]
 
-def test_pascals_triangle_5():
-    """asserts that the output is correct for n = 5."""
-    assert pascals_triangle(5) == [1, 4, 6, 4, 1]
+# myname = "sebastienfauque"
+# mynamelength = len(myname)
 
-def test_pascals_triangle_6():
-    """asserts that the output is correct for n = 6."""
-    assert pascals_triangle(6) == [1, 5, 10, 10, 5, 1]
+# pascals_triangle(mynamelength)
+# row 0  [1]
+# row 1  [1, 1]
+# row 2  [1, 2, 1]
+# row 3  [1, 3, 3, 1]
+# row 4  [1, 4, 6, 4, 1]
+# row 5  [1, 5, 10, 10, 5, 1]
+# row 6  [1, 6, 15, 20, 15, 6, 1]
+# row 7  [1, 7, 21, 35, 35, 21, 7, 1]
+# row 8  [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# row 9  [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+# row 10  [1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1]
+# row 11  [1, 11, 55, 165, 330, 462, 462, 330, 165, 55, 11, 1]
+# row 12  [1, 12, 66, 220, 495, 792, 924, 792, 495, 220, 66, 12, 1]
+# row 13  [1, 13, 78, 286, 715, 1287, 1716, 1716, 1287, 715, 286, 78, 13, 1]
+# row 14  [1, 14, 91, 364, 1001, 2002, 3003, 3432, 3003, 2002, 1001, 364, 91, 14, 1]
+
+#program output example for n = 3
+# Stack frame for recursion depth 3.
+# row 0  [1]
+# row 1  [1, 1]
+# row 2  [1, 2, 1]
--- lab2/sebastienfauqueLab2.py
+++ lab2/sebastienfauqueLab2.py
@@ -1,33 +1,21 @@
-# Attempt 1
-# def pascals_triangle(n: int, row_num = 0, old_row: list[int] = []) -> list:
-#     """Implementation of Pascal's Triangle using recursion."""
-#     if n <= 0:
-#         """Edge case."""
-#         return None
-#     if row_num == n:
-#         """Base case."""
-#         return None
-#
-#
-#     new_row = []
-#     new_row.append(1)
-#     if row_num > 1:
-#
-#     print(f"row {row_num}", new_row)
-#     return pascals_triangle(n, row_num + 1, new_row)
-
-# Attempt 2
 def pascals_triangle(n: int, current_triangle: list = []):
+    """Recursive implementation of Pascal's triangle.
+    Params:
+        n (int): Number of rows in the triangle.
+        current_triangle (list of lists[int): current rows of the triangle
+    """
+
+    # Ending condition
     if len(current_triangle) == n:
-        for row in current_triangle:
-            print(row)
+        for idx, row in enumerate(current_triangle):
+            print(f"row {idx} ", row)
         return None
 
     # implement rules for adding a layer
     if len(current_triangle):
         max_row: list[int] = current_triangle[-1]
         new_row: list[int] = [1]
-        for idx, item in enumerate(max_row):
+        for idx in range(len(max_row)):
 
             try:
                 right: int = max_row[idx + 1]
@@ -38,18 +26,10 @@ def pascals_triangle(n: int, current_triangle: list = []):
                 current_triangle.append(new_row)
                 return pascals_triangle(n, current_triangle)
 
-            #print(new_row)
-
-            # if right == 1:
-            #     # end the row and append to current triangle
-            #     current_triangle.append(new_row)
-            #     return pascals_triangle(n, current_triangle)
+    # Solve the most simple case.
     return pascals_triangle(n, [[1]])
 
-print("TEST CASES ----------------------")
-for n in range(1, 7):
-    print(f"Case {n}: ")
-    pascals_triangle(n)
+
 
 
 
